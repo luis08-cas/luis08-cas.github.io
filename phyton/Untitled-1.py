bf372e4336216514b726bf890e83a043fcce97d3
luis08-cas <luisfernando.rua83@gmail.com>
2025-11-28 21:08:41 -0500

    Tareas

---
+++ phyton/Untitled-1.py
@@ -0,0 +1,12 @@
+print("Simulacion de los pasos de una tortuga") 
+
+Pasos_abajo = int(input("¿Cuántos pasos quieres que baje la tortuga? "))
+
+print("La tortuga baja", Pasos_abajo, "pasos.")
+
+print("🏁") 
+
+for i in range(Pasos_abajo):
+ print("|")
+
+print("🐢")
